98d9b3536471406212e374035dd5863fb50d2fac
Irvan Ariyanto <irvan.ariyanto@gdplabs.id>
2022-12-21 06:35:30 +0700

    connect to mysql using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomersModule } from './customers/customers.module';
+import entities from './typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'tutorial_db',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/users/services/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { SerializedUser, User } from 'src/users/types';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      username: 'anson',
+      password: 'anson',
+    },
+    { id: 2, username: 'danny', password: 'danny' },
+    { id: 3, username: 'deren', password: 'deren' },
+    { id: 4, username: 'samantha', password: 'samantha' },
+  ];
+
+  getUsers() {
+    return this.users.map((user) => new SerializedUser(user));
+  }
+
+  getUserByUsername(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+}
